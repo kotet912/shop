1b17825df1bcf6ef7f811dbb034be3483d2478f7
kotet912 <37473076+kotet912@users.noreply.github.com>
2023-04-10 13:04:15 +0300

    добавление поиска в каталоге

--- src/store.js
+++ src/store.js
@@ -1,83 +1,92 @@
 import { createStore } from 'vuex';
 
 const store = createStore({
-    state: {
-        products: [
-            {
-                id: '1',
-                image: 'wondicon.png',
-                title: 'Product 1',
-                description: 'Description 1',
-                price: 10,
-                count: 0,
-            },
-            {
-                id: '2',
-                image: 'wondicon.png',
-                title: 'Product 2',
-                description: 'Description 2',
-                price: 15,
-                count: 0,
-            },
-            {
-                id: '3',
-                image: 'wondicon.png',
-                title: 'Product 3',
-                description: 'Description 3',
-                price: 20,
-                count: 0,
-            },
-            {
-                id: '4',
-                image: 'wondicon.png',
-                title: 'Product 4',
-                description: 'Description 4',
-                price: 25,
-                count: 0,
-            },
-            {
-                id: '5',
-                image: 'wondicon.png',
-                title: 'Product 5',
-                description: 'Description 5',
-                price: 30,
-                count: 0,
-            },
-            {
-                id: '6',
-                image: 'logo.png',
-                title: 'Product 6',
-                description: 'Description 6',
-                price: 35,
-                count: 0,
-            },
-        ],
-        sortOptions: {
-            priceUp: {
-                title: 'Цена по возрастанию',
-                value: 'priceUp',
-                sortFunc: (a, b) => a.price - b.price,
-            },
-            priceDown: {
-                title: 'Цена по убыванию',
-                value: 'priceDown',
-                sortFunc: (a, b) => b.price - a.price,
-            },
-        },
-        sortValue: 'priceUp',
+  state: {
+    products: [
+      {
+        id: '1',
+        image: 'wondicon.png',
+        title: 'Product 1',
+        description: 'Description 1',
+        price: 10,
+        count: 0,
+      },
+      {
+        id: '2',
+        image: 'wondicon.png',
+        title: 'Product 2',
+        description: 'Description 2',
+        price: 15,
+        count: 0,
+      },
+      {
+        id: '3',
+        image: 'wondicon.png',
+        title: 'Product 3',
+        description: 'Description 3',
+        price: 20,
+        count: 0,
+      },
+      {
+        id: '4',
+        image: 'wondicon.png',
+        title: 'Product 4',
+        description: 'Description 4',
+        price: 25,
+        count: 0,
+      },
+      {
+        id: '5',
+        image: 'wondicon.png',
+        title: 'Product 5',
+        description: 'Description 5',
+        price: 30,
+        count: 0,
+      },
+      {
+        id: '6',
+        image: 'logo.png',
+        title: 'Product 6',
+        description: 'Description 6',
+        price: 35,
+        count: 0,
+      },
+    ],
+    sortOptions: {
+      priceUp: {
+        title: 'Цена по возрастанию',
+        value: 'priceUp',
+        sortFunc: (a, b) => a.price - b.price,
+      },
+      priceDown: {
+        title: 'Цена по убыванию',
+        value: 'priceDown',
+        sortFunc: (a, b) => b.price - a.price,
+      },
     },
-    mutations: {
-        setFilterValue(state, value) {
-            state.sortValue = value;
-        },
+    sortValue: 'priceUp',
+    searchQuery: '',
+  },
+  mutations: {
+    setFilterValue(state, value) {
+      state.sortValue = value;
     },
-    actions: {},
-    getters: {
-        sortedProducts(state) {
-            const sortOption = state.sortOptions[state.sortValue];
-            return [...state.products].sort(sortOption.sortFunc);
-        },
+    setSearchQuery(state, value) {
+      state.searchQuery = value.toLowerCase();
     },
+  },
+  actions: {},
+  getters: {
+    sortedProducts(state) {
+      const sortOption = state.sortOptions[state.sortValue];
+      return [...state.products].sort(sortOption.sortFunc);
+    },
+    filteredProducts(state, getters) {
+      return getters.sortedProducts.filter((product) => {
+        return product.title.toLowerCase().includes(state.searchQuery);
+      });
+    },
+  },
 });
 
 export default store;
--- src/views/pageCatalog.vue
+++ src/views/pageCatalog.vue
@@ -1,43 +1,42 @@
 <template>
-    <main class="pageCatalog">
-        <div class="pageCatalog__box">
-            <input
-                type="text"
-                placeholder="Поиск"
-                class="pageCatalog__box__input"
-            />
-        </div>
-        <span />
-        <div class="pageCatalog__container">
-            <div class="pageCatalog__container__filter">
-                Сортировка:
-                <select
-                    v-model="filterValue"
-                    @change="setFilterValue(filterValue)"
-                >
-                    <option
-                        v-for="option in sortOptions"
-                        :key="option.value"
-                        :value="option.value"
-                    >
-                        {{ option.title }}
-                    </option>
-                </select>
-            </div>
-            <div class="pageCatalog__container__products">
-                <vCard
-                    class="pageCatalog__container__vCard"
-                    v-for="item in sortedProducts"
-                    :key="item.id"
-                    :id="item.id"
-                    :image="item.image"
-                    :title="item.title"
-                    :description="item.description"
-                    :price="item.price"
-                />
-            </div>
-        </div>
-    </main>
+  <main class="pageCatalog">
+    <div class="pageCatalog__box">
+      <input
+        type="text"
+        placeholder="Поиск"
+        v-model="searchQuery"
+        class="pageCatalog__box__input"
+        @change="setSearchQuery(searchQuery)"
+      />
+    </div>
+    <span />
+    <div class="pageCatalog__container">
+      <div class="pageCatalog__container__filter">
+        Сортировка:
+        <select v-model="filterValue" @change="setFilterValue(filterValue)">
+          <option
+            v-for="option in sortOptions"
+            :key="option.value"
+            :value="option.value"
+          >
+            {{ option.title }}
+          </option>
+        </select>
+      </div>
+      <div class="pageCatalog__container__products">
+        <vCard
+          class="pageCatalog__container__vCard"
+          v-for="item in filteredProducts"
+          :key="item.id"
+          :id="item.id"
+          :image="item.image"
+          :title="item.title"
+          :description="item.description"
+          :price="item.price"
+        />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -45,29 +44,44 @@ import { mapState, mapGetters, mapMutations } from 'vuex';
 import vCard from '@/components/vCard/vCard.vue';
 
 export default {
-    name: 'pageCatalog',
-    components: {
-        vCard,
-    },
+  name: 'pageCatalog',
+  components: {
+    vCard,
+  },
 
-    computed: {
-        ...mapState({
-            sortOptions: (state) => state.sortOptions,
-        }),
-        ...mapGetters(['sortedProducts']),
-    },
+  computed: {
+    ...mapState({
+      sortOptions: (state) => state.sortOptions,
+      sortValue: (state) => state.sortValue,
+    }),
+    ...mapGetters(['sortedProducts']),
+    ...mapGetters(['filteredProducts']),
 
-    data() {
-        return {
-            filterValue: 'priceUp',
-        };
-    },
+    searchQuery: {
+      get: function() {
+        return this.$store.state.searchQuery;
+      },
+      set: function(newValue) {
+        this.$store.commit('setSearchQuery', newValue);
+      }
+    }
+  },
 
-    mounted() {},
+  data() {
+    return {
+      // searchQuery: '',
+      filterValue: 'priceUp',
+    };
+  },
 
-    methods: {
-        ...mapMutations(['setFilterValue']),
-    },
+  mounted() {
+    this.filterValue = this.sortValue;
+  },
+
+  methods: {
+    ...mapMutations(['setFilterValue']),
+    ...mapMutations(['setSearchQuery']),
+  },
 };
 </script>
 
